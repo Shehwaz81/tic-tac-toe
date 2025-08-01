d512076e2562d1ae6876334a7f4fad2f40ed106c
Shehwaz81 <shehwazlongia@gmail.com>
2025-08-01 15:42:31 -0400

    added next counter styling

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,6 @@
       newsquares[i] = (isx ? "X" : "O")
       setSquares(newsquares) // changing reference, causing a component rerender, you cant do sqaures = newsqaures as it is considered mutating state
       setIsx(!isx)
-      if (is_winner(newsquares)) console.log("winner")
     }
 
     if (status) {
@@ -57,24 +56,26 @@
     }
 
     return (
-      <div className='container'>
+      <>
         <div className='status'> {status}</div>
-        <div className='row'>
-          <Square value={squares[0]} onSquareClick = {() => {handleClick(0)}}/>
-          <Square value={squares[1]} onSquareClick = {() => {handleClick(1)}}/>
-          <Square value={squares[2]} onSquareClick = {() => {handleClick(2)}}/>
+        <div className='container'>
+          <div className='row'>
+            <Square value={squares[0]} onSquareClick = {() => {handleClick(0)}}/>
+            <Square value={squares[1]} onSquareClick = {() => {handleClick(1)}}/>
+            <Square value={squares[2]} onSquareClick = {() => {handleClick(2)}}/>
+          </div>
+          <div className='row'>
+            <Square value={squares[3]} onSquareClick = {() => {handleClick(3)}}/>
+            <Square value={squares[4]} onSquareClick = {() => {handleClick(4)}}/>
+            <Square value={squares[5]} onSquareClick = {() => { handleClick(5)}}/>
+          </div>
+          <div className='row'>
+            <Square value={squares[6]} onSquareClick = {() => {handleClick(6)}}/>
+            <Square value={squares[7]} onSquareClick = {() => {handleClick(7)}}/>
+            <Square value={squares[8]} onSquareClick = {() => {handleClick(8)}}/>
+          </div>
         </div>
-        <div className='row'>
-          <Square value={squares[3]} onSquareClick = {() => {handleClick(3)}}/>
-          <Square value={squares[4]} onSquareClick = {() => {handleClick(4)}}/>
-          <Square value={squares[5]} onSquareClick = {() => { handleClick(5)}}/>
-        </div>
-        <div className='row'>
-          <Square value={squares[6]} onSquareClick = {() => {handleClick(6)}}/>
-          <Square value={squares[7]} onSquareClick = {() => {handleClick(7)}}/>
-          <Square value={squares[8]} onSquareClick = {() => {handleClick(8)}}/>
-        </div>
-      </div>
+      </>
     )
   }
 
